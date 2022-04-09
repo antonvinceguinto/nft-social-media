faaa200a9130d9623d64b2b8cc848549cf5ba750
Anton Guinto <antonvinceguinto@gmail.com>
2022-04-09 17:53:01 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:@next/next/recommended', 'airbnb', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-console': 1,
+    'import/no-unresolved': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
+        '': 'never',
+      },
+    ],
+    'jsx-quotes': [2, 'prefer-single'],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.tsx', '.ts'],
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'latest',
+    },
+  },
+};
